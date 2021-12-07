7add203c3036e44dc9f24cf8e92e4f43f7df0d94
Kevin <kevvbautista@gmail.com>
2021-12-07 12:08:40 +0800

    added activestorage

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require "rails"
 require "carrierwave"
+require "activestorage"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
